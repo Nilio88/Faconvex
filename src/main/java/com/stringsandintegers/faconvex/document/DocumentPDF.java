f0a167f896e730532666c631f0c8b55517a10d1e
Daniele Porcelli <d.porcelli@studenti.uniba.it>
2017-06-18 11:44:21 +0200

    -Added PDFBox-Layout API; -Used the PDFBox-Layout API to create the PDF document in DocumentPDF class.

--- src/main/java/com/stringsandintegers/faconvex/document/DocumentPDF.java
+++ src/main/java/com/stringsandintegers/faconvex/document/DocumentPDF.java
@@ -6,12 +6,14 @@ package com.stringsandintegers.faconvex.document;
 
 import com.stringsandintegers.faconvex.model.Message;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import rst.pdfbox.layout.elements.Paragraph;
+import rst.pdfbox.layout.text.Alignment;
+import rst.pdfbox.layout.text.Constants;
+import rst.pdfbox.layout.text.BaseFont;
 
 /**
  * This class creates a PDF file
@@ -26,10 +28,7 @@ class DocumentPDF implements Document {
      * @throws IOException if an error occured during the initialization of the stream. 
      */
     DocumentPDF() throws IOException {
-        pdfDocument = new PDDocument();
-        page = new PDPage();
-        pdfDocument.addPage(page);
-        content = new PDPageContentStream(pdfDocument, page);
+        pdfDocument = new rst.pdfbox.layout.elements.Document(Constants.A4, LEFT_MARGIN, RIGHT_MARGIN, TOP_MARGIN, BOTTOM_MARGIN);
     }
     
     public void addTitle(String title) throws IOException {
@@ -48,70 +47,60 @@ class DocumentPDF implements Document {
             }
         }
         
-        //Get information for centering the title
-        int marginTop = 20;
-        PDFont titleFont = PDType1Font.HELVETICA_BOLD;
-        int titleFontSize = 25;
-        float titleWidth = titleFont.getStringWidth(outTitle) / 1000 * titleFontSize;
-        float titleHeight = titleFont.getFontDescriptor().getFontBoundingBox().getHeight() / 1000 * titleFontSize;
-        
         //Print the title in the PDF document
-        content.beginText();
-        content.setFont(titleFont, titleFontSize);
-        content.moveTextPositionByAmount((page.getMediaBox().getWidth() - titleWidth) / 2, page.getMediaBox().getHeight() - marginTop - titleHeight);
-        content.showText(outTitle);
-        content.newLine();
-        content.newLine();
-        content.endText();
+        Paragraph titleParagraph = new Paragraph();
+        titleParagraph.addText(outTitle + "\n", TITLE_FONT_SIZE, PDType1Font.HELVETICA_BOLD);
+        titleParagraph.setAlignment(Alignment.Center);
+        pdfDocument.add(titleParagraph);
     }
     
     public void addOwnerMessage(Message message) throws IOException {
         //Convert the message into a string to be printed on the PDF document
-        String outMessage = message.getSender() + " at " + message.getDate() + "\n" + message.getMessage();
+        String outMessage = message.getSender() + " at _" + message.getDate() + "_\n" + message.getMessage() + "\n";
         
-        //Initialize the font size and type for the message
-        PDFont messageFont = PDType1Font.TIMES_ROMAN;
+        //Initialize the font size for the message
         int messageFontSize = 16;
         
-        //Initialize information for right-aligning the message
-        float messageWidth = (messageFont.getStringWidth(outMessage) / 1000.0f) * messageFontSize;
-        
         //Print the message on the PDF document
-        content.beginText();
-        content.setFont(messageFont, messageFontSize);
-        content.moveTextPositionByAmount(-messageWidth, 0);
-        content.showText(outMessage);
-        content.moveTextPositionByAmount(messageWidth, 0);
-        content.newLine();
-        content.newLine();
-        content.endText();
+        Paragraph ownerParagraph = new Paragraph();
+        ownerParagraph.addMarkup(outMessage, messageFontSize, BaseFont.Times);
+        ownerParagraph.setMaxWidth(MESSAGE_BOX_SIZE);   //Set the message box size
+        ownerParagraph.setAlignment(Alignment.Right);
+        pdfDocument.add(ownerParagraph);
     }
     
     public void addRecipientMessage(Message message) throws IOException {
         //Convert the message into a string to be printed on the PDF document
-        String outMessage = message.getSender() + " at " + message.getDate() + "\n" + message.getMessage();
+        String outMessage = message.getSender() + " at _" + message.getDate() + "_\n" + message.getMessage() + "\n";
         
         //Initialize the font size and type for the message
-        PDFont messageFont = PDType1Font.TIMES_ROMAN;
         int messageFontSize = 16;
         
         //Print the message on the PDF document (left-aligned)
-        content.beginText();
-        content.setFont(messageFont, messageFontSize);
-        content.showText(outMessage);
-        content.newLine();
-        content.newLine();
-        content.endText();
+        Paragraph recipientParagraph = new Paragraph();
+        recipientParagraph.addMarkup(outMessage, messageFontSize, BaseFont.Times);
+        recipientParagraph.setMaxWidth(MESSAGE_BOX_SIZE);   //Set the message box size
+        recipientParagraph.setAlignment(Alignment.Left);
+        pdfDocument.add(recipientParagraph);
     }
     
     public void close(File outputFile) throws IOException {
-        content.close();
-        pdfDocument.save(outputFile);
-        pdfDocument.close();
+        pdfDocument.save(new FileOutputStream(outputFile));
     }
     
     //Instance variables
-    private PDDocument pdfDocument;
-    private PDPageContentStream content;
-    private PDPage page;
+    private rst.pdfbox.layout.elements.Document pdfDocument;
+    
+    //Class constants
+    //Document margins
+    private static final int LEFT_MARGIN = 40;
+    private static final int RIGHT_MARGIN = 60;
+    private static final int TOP_MARGIN = 40;
+    private static final int BOTTOM_MARGIN = 60;
+    
+    //Font sizes
+    private static final int TITLE_FONT_SIZE = 24;
+    
+    //Messsages box size
+    private static final int MESSAGE_BOX_SIZE = 200;
 }
